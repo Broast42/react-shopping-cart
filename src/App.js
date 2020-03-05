2ea5ccebb65abb64163d12d50419ab838cc235d6
rdowd <rjarrettdowd@yahoo.com>
2020-03-04 21:49:32 -0500

    set up ProductContect. provider in app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -14,22 +15,25 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
-		setCart({...state, item});
+		setCart([...state, item]);
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</div>	
+		</ProductContext.Provider>
+		
 	);
 }
 
